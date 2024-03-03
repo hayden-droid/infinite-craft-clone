6b2de8ab73044eda601e461ddc97542e54d4f387
Descendo The Monkei <61548447+hayden-droid@users.noreply.github.com>
2024-03-03 17:55:04 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Infinite Crafter Clone",
+  title: "Memeitizer Infinite Crafter",
   description: "Combine elements to create new ones, forever!",
 };
 
